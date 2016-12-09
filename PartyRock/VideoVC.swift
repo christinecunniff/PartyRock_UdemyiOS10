49f1a6ff39a210652ef9caa19bcc485f3452f1ca
Christine Cunniff <christinecunniff@gmail.com>
2016-12-09 14:16:52 -0500

    Created a variable for partyRock webview object.

--- PartyRock/VideoVC.swift
+++ PartyRock/VideoVC.swift
@@ -1,24 +1,26 @@
-//
-//  VideoVC.swift
-//  PartyRock
-//
-//  Created by Christine Cunniff on 12/8/16.
-//  Copyright © 2016 Christine Cunniff. All rights reserved.
-//
-
 import UIKit
 
 class VideoVC: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    private var _partyRock: PartyRock!
 
+    var partyRock: PartyRock {
+        get {
+            return _partyRock
+        } set {
+            _partyRock = newValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
